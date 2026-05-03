476e411ee1fbcc87fe304a42f171bedafaac379c
felipezy23 <de.ferreira@escola.pr.gov.br>
2026-05-03 12:45:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,5 @@ const botao = document.getElementById("botao");
 const mensagem = document.getElementById("mensagem");
 
 botao.addEventListener("click", () => {
-
-    mensagem.innerHTML =
-    "Você é fenomenal, literalmente a coisa mais especial que aconteceu na minha vida. Sem você eu não vivo meu amor, quero você pra sempre ❤️";
-
+    mensagem.innerHTML = "Você é fenomenal, literalmente a coisa mais especial que aconteceu na minha vida. Sem você eu não vivo meu amor, quero você pra sempre ❤️";
 });
-
