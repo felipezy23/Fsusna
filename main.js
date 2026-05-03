5c7cf531cb9e5f2742824981842f4109f12719cd
felipezy23 <de.ferreira@escola.pr.gov.br>
2026-05-03 15:44:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const botao = document.getElementById("botao");
 const mensagem = document.getElementById("mensagem");
 
 botao.addEventListener("click", () => {
-    mensagem.innerHTML = "Você é fenomenal, literalmente a coisa mais especial que aconteceu na minha vida. Sem você eu não vivo meu amor, quero você pra sempre ❤️";
+    mensagem.innerHTML = "Você é fenomenal, literalmente a coisa mais especial que aconteceu na minha vida. Sem você eu não vivo meu amor, quero você pra sempre, eu te amo tanto que meu coração anseia por você. É um amor incondicional que eu nunca senti antes, eu te amo muito e sempre vou amar, quero estar com você até a eternidade. Meu primeiro e grande amor isso é para você ana eu te amo❤️";
 });
